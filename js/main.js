7aedb8e790364c1f6f821ad87f7594921edac884
Tobias Kiehnlein <tobias@kiehnlein.com>
2020-09-03 21:34:02 +0200

    graphical update

--- js/main.js
+++ js/main.js
@@ -1,75 +1,30 @@
-import ScrollMagic from 'scrollmagic';
-import {TimelineMax, TweenMax} from "gsap";
-import {ScrollMagicPluginGsap} from "scrollmagic-plugin-gsap";
+import enterView from './sticky'
 
-ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
-
-//region ScrollMagic
-const intro = document.querySelector('.intro');
-const video = intro.querySelector('video');
-const title = intro.querySelector('h1');
-const addressInfo = intro.querySelectorAll('.contact-info > *');
-
-const section = document.querySelector('section');
-const end = section.querySelector('h1')
-
-const controller = new ScrollMagic.Controller();
-
-const scene = new ScrollMagic.Scene({
-  duration: 1000,
-  triggerElement: intro,
-  triggerHook: 0
-})
-  // .addIndicators()
-  .setPin(intro)
-  .addTo(controller);
-
-
-// Text animation
-const textAnim = TweenMax.to(title, 0.3, {transform: 'translateY(-40vh)'})
-new ScrollMagic.Scene({
-  duration: 300,
-  triggerElement: intro,
-  triggerHook: 0
-}).setTween(textAnim).addTo(controller);
-
-addressInfo.forEach(infoElt => {
-  const textAnim = TweenMax.to(infoElt, 1, {transform: 'translateX(0)'})
-  new ScrollMagic.Scene({
-    duration: 1000,
-    triggerElement: intro,
-    triggerHook: -0.5
-  }).setTween(textAnim).addTo(controller);
+enterView({
+  selector: 'section',
+  enter: function (el) {
+    el.classList.add('entered');
+  }
 })
 
-// Video animation
-let accelerationAmount = 0.1;
-let scrollPos = 0;
-let delay = 0;
+// lower numbers = faster playback
+const playbackConst = 1000,
+  // select video element
+  vid = document.getElementById('v0');
 
-// scene.on("update", e => video.currentTime = e.scrollPos / 250);
-scene.on("update", e => scrollPos = e.scrollPos / 250);
-
-setInterval(() => {
-  const difference = (scrollPos - delay) * accelerationAmount;
-  delay += difference;
-  video.currentTime = delay;
-}, 50)
-//endregion
+// dynamically set the page height according to video length
+vid.addEventListener('loadedmetadata', () => {
+  // setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px"; Wasn't necessary anymore... still a good way to figure out how large the sum of all sections should be
+  vid.pause()
+});
 
 
-//region PreviewElements
-document.querySelectorAll('.preview').forEach(elt => {
-  const sectionScene = new ScrollMagic.Scene({
-    duration: 1000,
-    triggerElement: elt,
-    triggerHook: 0.8
-  })
-    // .addIndicators()
-    .addTo(controller);
+// Use requestAnimationFrame for smooth playback
+const scrollPlay = () => { 
+  vid.currentTime = window.pageYOffset / playbackConst;
 
-  sectionScene.on('enter', () => elt.classList.add('visible'));
-  sectionScene.on('leave', () => elt.classList.remove('visible'));
-})
-//endregion
+  window.requestAnimationFrame(scrollPlay);
+}
+
+window.requestAnimationFrame(scrollPlay);
 
