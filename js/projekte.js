5f2b1ead3b9f8a82261587104c5241e50a405e46
Tobias Kiehnlein <tobias@kiehnlein.com>
2020-09-04 00:08:38 +0200

    Add projects

---
+++ js/projekte.js
@@ -0,0 +1,23 @@
+const polaroid = document.querySelectorAll('.polaroid');
+const textContainer = document.querySelector('.text-container')
+
+const imgClick = (img) => {
+  img.classList.toggle('active')
+  textContainer.classList.toggle('visible')
+
+  if (img.classList.contains('active')) {
+    document.querySelectorAll('.text').forEach(text => text.style.visibility = 'hidden')
+    document.querySelectorAll('.polaroid').forEach(text => text.style.zIndex = '0')
+    document.querySelector(`.text-container .${img.dataset.id}`).style.visibility = 'visible'
+    img.style.zIndex = '25'
+  }
+}
+
+polaroid.forEach(elt => {
+  elt.addEventListener('click', () => imgClick(elt))
+})
+
+textContainer.addEventListener('click', () => {
+  document.querySelectorAll('.active').forEach(x => x.classList.remove('active'))
+  textContainer.classList.remove('visible')
+})
