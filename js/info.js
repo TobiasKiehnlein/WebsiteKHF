70297067583b59f4197411b394f0d75cefdb0048
Tobias Kiehnlein <tobias@kiehnlein.com>
2020-09-15 12:57:15 +0200

    Styles...

--- js/info.js
+++ js/info.js
@@ -1,13 +1,12 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    const cardList = document.querySelector('.card-list');
-    cardList.classList.add('mobile');
+	const cardList = document.querySelector('.card-list');
+	cardList.classList.add('mobile');
 
-    const cards = cardList.querySelectorAll('.card');
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            cards.forEach(c => c !== card && c.classList.remove('card-active'))
-            card.classList.toggle('card-active')
-
-        })
-    })
+	const cards = cardList.querySelectorAll('.card');
+	cards.forEach(card => {
+		card.addEventListener('click', () => {
+			cards.forEach(c => c !== card && c.classList.remove('card-active'));
+			card.classList.toggle('card-active');
+		});
+	});
 }
--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ import enterView from './sticky'
 const isIOS = iOS();
 
 enterView({
-  selector: 'section',
-  enter: function (el) {
-    el.classList.add('entered');
-  }
+	selector: 'section',
+	enter: function (el) {
+		el.classList.add('entered');
+	}
 })
 
 // lower numbers = faster playback
@@ -16,30 +16,30 @@ const vid = document.getElementById('v0');
 
 // dynamically set the page height according to video length
 vid.addEventListener('loadedmetadata', () => {
-  if (!isIOS) return;
-  vid.pause()
+	if (!isIOS) return;
+	vid.pause()
 });
 
 
 // Use requestAnimationFrame for smooth playback
 const scrollPlay = () => {
-  if (!isIOS) return;
-  vid.currentTime = window.pageYOffset / playbackConst;
+	if (!isIOS) return;
+	vid.currentTime = window.pageYOffset / playbackConst;
 
-  window.requestAnimationFrame(scrollPlay);
+	window.requestAnimationFrame(scrollPlay);
 }
 
 window.requestAnimationFrame(scrollPlay);
 
 function iOS() {
-  return [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod'
-    ].includes(navigator.platform)
-    // iPad on iOS 13 detection
-    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+	return [
+			'iPad Simulator',
+			'iPhone Simulator',
+			'iPod Simulator',
+			'iPad',
+			'iPhone',
+			'iPod'
+		].includes(navigator.platform)
+		// iPad on iOS 13 detection
+		|| (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 }
