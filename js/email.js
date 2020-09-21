82f64a58cd74bedae3d86669b250ff19e5c01dc2
Tobias Kiehnlein <tobias@kiehnlein.com>
2020-09-21 19:27:04 +0200

    Another test for deploy action

--- js/email.js
+++ js/email.js
@@ -3,7 +3,7 @@ const textArea = document.querySelector('textarea')
 
 const callback = e => {
   e.preventDefault();
-  window.open(`mailto:info@khfollner.de?subject=${encodeURIComponent("Kontakt - Website")}&body=${encodeURIComponent(textArea.value)}`)
+  window.location = `mailto:info@khfollner.de?subject=${encodeURIComponent("Kontakt - Website")}&body=${encodeURIComponent(textArea.value)}`
 }
 
 if (ele.addEventListener) {
