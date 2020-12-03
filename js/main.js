4476a83cb17827cce892afa4a0d0f729805c54ea
Tobias Kiehnlein <tobias@kiehnlein.com>
2020-12-03 11:49:22 +0100

    cleanup and finalisation

--- js/main.js
+++ js/main.js
@@ -1,45 +1,55 @@
-import enterView from './sticky'
+const elements = document.querySelectorAll('.element');
 
-const isIOS = iOS();
+elements.forEach(element => {
+	element.addEventListener('click', evt => {
+		location.href = element.querySelector("a").href;
+	});
+});
 
-enterView({
-	selector: 'section',
-	enter: function (el) {
-		el.classList.add('entered');
-	}
-})
+setTimeout(() => elements.forEach(element => element.classList.add("visible")), 4000);
+
+// import enterView from './sticky'
+
+// const isIOS = iOS();
+
+// enterView({
+// 	selector: 'section',
+// 	enter: function (el) {
+// 		el.classList.add('entered');
+// 	}
+// })
 
 // lower numbers = faster playback
-const playbackConst = 1000;
+// const playbackConst = 1000;
 // select video element
-const vid = document.getElementById('v0');
+// const vid = document.getElementById('v0');
 
 // dynamically set the page height according to video length
-vid.addEventListener('loadedmetadata', () => {
-	if (!isIOS) return;
-	vid.pause()
-});
+// vid.addEventListener('loadedmetadata', () => {
+// 	if (!isIOS) return;
+// 	vid.pause()
+// });
 
 
 // Use requestAnimationFrame for smooth playback
-const scrollPlay = () => {
-	if (!isIOS) return;
-	vid.currentTime = window.pageYOffset / playbackConst;
+// const scrollPlay = () => {
+// if (!isIOS) return;
+// 	vid.currentTime = window.pageYOffset / playbackConst;
+//
+// 	window.requestAnimationFrame(scrollPlay);
+// }
+//
+// window.requestAnimationFrame(scrollPlay);
 
-	window.requestAnimationFrame(scrollPlay);
-}
-
-window.requestAnimationFrame(scrollPlay);
-
-function iOS() {
-	return [
-			'iPad Simulator',
-			'iPhone Simulator',
-			'iPod Simulator',
-			'iPad',
-			'iPhone',
-			'iPod'
-		].includes(navigator.platform)
-		// iPad on iOS 13 detection
-		|| (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-}
+// function iOS() {
+// 	return [
+// 			'iPad Simulator',
+// 			'iPhone Simulator',
+// 			'iPod Simulator',
+// 			'iPad',
+// 			'iPhone',
+// 			'iPod'
+// 		].includes(navigator.platform)
+// 		// iPad on iOS 13 detection
+// 		|| (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+// }
